48c8684c6c732281348de3e396c80f687a630158
SGrossett <selene_816@hotmail.com>
2023-08-02 16:29:56 -0400

    Bind the image sorce attributes to the sorce element so the images are pulled from the data file.

--- src/build/RobotBuilder.vue
+++ src/build/RobotBuilder.vue
@@ -1,34 +1,34 @@
 <template>
   <div>
-    <div class="top-row">
-      <div class="top part">
-        <img src="./images/head-big-eye.png" title="head"/>
-        <button class="prev-selector">&#9668;</button>
-        <button class="next-selector">&#9658;</button>
+    <div class='top-row'>
+      <div class='top part'>
+        <img v-bind:src='availableParts.heads[0].src' title='head'/>
+        <button class='prev-selector'>&#9668;</button>
+        <button class='next-selector'>&#9658;</button>
       </div>
     </div>
-    <div class="middle-row">
-      <div class="left part">
-        <img src="./images/arm-articulated-claw.png" title="left arm"/>
-        <button class="prev-selector">&#9650;</button>
-        <button class="next-selector">&#9660;</button>
+    <div class='middle-row'>
+      <div class='left part'>
+        <img v-bind:src='availableParts.arms[0].src' title='torso'/>
+        <button class='prev-selector'>&#9650;</button>
+        <button class='next-selector'>&#9660;</button>
       </div>
-      <div class="center part">
-        <img src="./images/torso-flexible-gauged.png" title="left arm"/>
-        <button class="prev-selector">&#9668;</button>
-        <button class="next-selector">&#9658;</button>
+      <div class='center part'>
+        <img v-bind:src='availableParts.torsos[0].src' title='left arm'/>
+        <button class='prev-selector'>&#9668;</button>
+        <button class='next-selector'>&#9658;</button>
       </div>
-      <div class="right part">
-        <img src="./images/arm-dual-claw.png" title="left arm"/>
-        <button class="prev-selector">&#9650;</button>
-        <button class="next-selector">&#9660;</button>
+      <div class='right part'>
+        <img v-bind:src='availableParts.arms[0].src' title='right arm'/>
+        <button class='prev-selector'>&#9650;</button>
+        <button class='next-selector'>&#9660;</button>
       </div>
     </div>
-    <div class="bottom-row">
-      <div class="bottom part">
-        <img src="./images/base-single-wheel.png" title="left arm"/>
-        <button class="prev-selector">&#9668;</button>
-        <button class="next-selector">&#9658;</button>
+    <div class='bottom-row'>
+      <div class='bottom part'>
+        <img v-bind:src='availableParts.bases[0].src' title='base'/>
+        <button class='prev-selector'>&#9668;</button>
+        <button class='next-selector'>&#9658;</button>
       </div>
     </div>
   </div>
